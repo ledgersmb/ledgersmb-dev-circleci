c989abe9eb484994c3637e322cc1a561f1117a80
Yves Lavoie <ylavoie@yveslavoie.com>
2020-09-02 11:14:32 -0400

    Set proper security for Postgres

--- postgres/lsmb-dev-init.sh
+++ postgres/lsmb-dev-init.sh
@@ -7,3 +7,10 @@ sed -i -e '
 s/^#fsync = on/fsync = off/;
 s/^#synchronous_commit = on/synchronous_commit = off/;
 ' $pgdata/postgresql.conf
+
+cat > "$pgdata/pg_hba.conf" <<'__END__'
+local   all             postgres                                trust
+host    all             postgres        127.0.0.1/32            trust
+host    all             postgres        ::1/128                 trust
+host    all             all             all                     md5
+__END__
